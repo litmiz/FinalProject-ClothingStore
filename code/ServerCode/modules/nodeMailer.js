b0d39ecb57205e6e4a4e6b67233e16a9f2a469de
litmiz <litmiz07@gmail.com>
2021-01-16 15:14:34 +0200

    Add nodemailer and send emails when order status changes
    Also add .env

---
+++ code/ServerCode/modules/nodeMailer.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
+
+exports.sendMail = function(to, subject, text) {
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: to,
+    subject: subject,
+    text: text
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
--- code/ServerCode/routes/orders.js
+++ code/ServerCode/routes/orders.js
@@ -1,7 +1,10 @@
 require("../data/database");
 const express = require("express");
+const { findOne } = require("../models/orders");
 const router = express.Router();
 const orderModel = require("../models/orders");
+const userModel = require("../models/users");
+const nodeMailer = require("../modules/nodeMailer");
 const itemsLimit = 10;
 
 router.get("/myOrders", (req, res) => {
@@ -68,14 +71,16 @@ router.post("/shoppingBag", (req, res) => {
             const newOrder = new orderModel({
                 userId: req.body.userId, // TODO: use token
                 items: req.body.items,
-                status: req.body.status,
+                status: "inBag",
                 amountToPay: req.body.amountToPay,
                 country: req.body.country,
                 city: req.body.city,
                 address: req.body.address,
                 payment: req.body.payment,
             })
-            newOrder.save().then(() => res.send(`The order is saved in the DB`));
+            newOrder.save().then(() => {
+                res.send(`The order is saved in the DB`);
+            });
         })
 });
 
@@ -85,14 +90,33 @@ router.put('/editMyOrder', (req, res) => {
         if (error) {
             console.error("There is an error with the put request.");
         }
-        else if (foundOrder.userId == req.body.userId){
+        else if (foundOrder.userId == req.body.userId) {
             orderModel.findOneAndUpdate(query, {$set: {
                 status: req.body.status,}}, (error, data) => {
                 if (error) {
                     console.error("The is an error with the put request.");
                 }
                 else {
-                    res.send(data);
+                    res.send("The order has been updated.");
+                    if (req.body.status == 'ordered') {
+                        userModel.findOne({_id: req.body.userId}, (error, user) => {
+                            if (error) {
+                                console.error(`The is an error finding the user ${req.body.userId}.`);
+                            } else {
+                                nodeMailer.sendMail(user.email, "Order Confirmation",
+                                `Dear ${user.fullName}, your order ${foundOrder._id} has been received. The order will be delivered to you soon.`)
+                            }
+                        });
+                    } else if (req.body.status == 'cancelRequest') {
+                        userModel.findOne({_id: req.body.userId}, (error, user) => {
+                            if (error) {
+                                console.error(`The is an error finding the user ${req.body.userId}.`);
+                            } else {
+                                nodeMailer.sendMail(user.email, "Order Cancellation Request",
+                                `Dear ${user.fullName}, your order ${foundOrder._id} has been request to be cancelled. We will send you a cancel confirmation soon.`)
+                            }
+                        });
+                    }
                 }
             });
         };
@@ -103,12 +127,40 @@ router.put('/editMyOrder', (req, res) => {
 router.put('/editOrder', (req, res) => {
     const query = {_id: req.body._id};
     orderModel.findOneAndUpdate(query, {$set: {
-        status: req.body.status,}}, (error, data) => {
+        status: req.body.status,}}, (error, order) => {
         if (error) {
             console.error("The is an error with the put request.");
         }
         else {
-            res.send(data);
+            res.send("The order has been updated");
+            if (req.body.status == 'shipped') {
+                userModel.findOne({_id: order.userId}, (error, user) => {
+                    if (error) {
+                        console.error(`The is an error finding the user ${order.userId}.`);
+                    } else {
+                        nodeMailer.sendMail(user.email, "Order Shipped",
+                        `Dear ${user.fullName}, your order ${order._id} has been shipped to you. The order will soon arrive to you at ${order.address}, ${order.city}, ${order.country}.`)
+                    }
+                });
+            } else if (req.body.status == 'delivered') {
+                userModel.findOne({_id: order.userId}, (error, user) => {
+                    if (error) {
+                        console.error(`The is an error finding the user ${order.userId}.`);
+                    } else {
+                        nodeMailer.sendMail(user.email, "Order Delivered",
+                        `Dear ${user.fullName}, your order ${order._id} has been delivered to you. If you didn't receive the order, please contact us.`)
+                    }
+                });
+            } else if (req.body.status == 'cancelled') {
+                userModel.findOne({_id: order.userId}, (error, user) => {
+                    if (error) {
+                        console.error(`The is an error finding the user ${order.userId}.`);
+                    } else {
+                        nodeMailer.sendMail(user.email, "Order Cancelled",
+                        `Dear ${user.fullName}, your order ${order._id} has been cancelled. We hope to receive other orders from you in the future.`)
+                    }
+                });
+            }
         }
     });
 })
